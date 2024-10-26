9a3cd04371ebf3a7336edc3f247177aa9a5704a3
Samuel Sml <veramirandasamuel6@gmail.com>
2024-10-25 20:36:42 -0500

    feat: book models defined with inherint

---
+++ src/models/books/EBook.py
@@ -0,0 +1,10 @@
+from Book import Book
+
+class EBook(Book):
+
+    def __init__(self, id, title, author, pages, description, quantity, publicationYear):
+        super().__init__(id, title, author, pages, description, quantity, publicationYear)
+
+
+    def download(id):
+        pass
---
+++ src/models/books/PhysicalBook.py
@@ -0,0 +1,7 @@
+from Book import Book
+
+class PhysicalBook(Book):
+
+    def __init__(self, id, title, author, pages, description, quantity, publicationYear, isBorrowed):
+        self.isBorrowed = isBorrowed
+        super().__init__(id, title, author, pages, description, quantity, publicationYear)
