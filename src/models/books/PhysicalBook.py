22d9e44ed3d203f7873efca8919fb00d152bcaf7
Samuel Sml <veramirandasamuel6@gmail.com>
2024-11-01 17:51:46 -0500

    feat: create method to view data in format json

--- src/models/books/PhysicalBook.py
+++ src/models/books/PhysicalBook.py
@@ -1,7 +1,20 @@
 from .Book import Book
+import json
 
 class PhysicalBook(Book):
 
     def __init__(self, id, title, author, pages, description, quantity, publicationYear, isBorrowed):
         self.isBorrowed = isBorrowed
         super().__init__(id, title, author, pages, description, quantity, publicationYear)
+
+    def __str__(self):
+        return json.dumps({
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "pages": self.pages,
+            "description": self.description,
+            "quantity": self.quantity,
+            "publicationYear": self.publicationYear,
+            "isBorrowed": self.isBorrowed
+        }, indent=4)
