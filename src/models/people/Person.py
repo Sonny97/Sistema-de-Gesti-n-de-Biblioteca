e374bc8f5b8ce9a2b14b03de24e1817279ca1ad0
Samuel Sml <veramirandasamuel6@gmail.com>
2024-11-01 18:25:08 -0500

    refactor: add atributes

--- src/models/people/Person.py
+++ src/models/people/Person.py
@@ -1,11 +1,13 @@
 class Person:
 
-    def __init__(self, id, name, lastName, birthDate):
+    def __init__(self, id, name, lastName, birthDate, booksLimit, booksBorrowed):
         self.id = id
         self.name = name
         self.lastName = lastName
         self.birthDate = birthDate
+        self.booksLimit = booksLimit
+        self.booksBorrowed = booksBorrowed
 
 
     def describeUser(self):
-        return print(f"Name: {self.name}\nLast Name: {self.lastName}\nBirth Date: {self.birthDate}")
+        return print(f"Name: {self.name}\nLast Name: {self.lastName}\nBirth Date: {self.birthDate}\n Books Limit: {self.booksLimit} \n Books Borrowed: {self.booksBorrowed}")
--- src/models/people/Teacher.py
+++ src/models/people/Teacher.py
@@ -2,6 +2,6 @@ from .Person import Person
 
 class Teacher(Person):
 
-    def __init__(self, id, name, lastName, birthDate, borrowedBooks):
+    def __init__(self, id, name, lastName, birthDate, borrowedBooks, booksLimit, booksBorrowed):
         self.borrowedBooks = borrowedBooks
-        super().__init__(id, name, lastName, birthDate)
+        super().__init__(id, name, lastName, birthDate, booksLimit, booksBorrowed)
