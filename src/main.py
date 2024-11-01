5cedff6f2d0bba331d2bcdfc3790b412f800ed8f
Samuel Sml <veramirandasamuel6@gmail.com>
2024-11-01 15:59:12 -0500

    fix: add self param at lendBookMenu and returnBookMenu to fix bugs and add self to call returnBookMenu at line 67

--- src/main.py
+++ src/main.py
@@ -2,30 +2,30 @@ from models.books.EBook import EBook
 from models.books.PhysicalBook import PhysicalBook
 
 class main:
-    
-    def lendBookMenu():
+
+    def lendBookMenu(self):
         print(""" como deseas buscar el libro a prestar?
               1. para buscar por Id.
               2. para buscar por titulo.
               3. para buscar por author.
               *. para volver al menu anterior.
               """)
-        
+
         eleccion = int(input())
 
         if(eleccion == 1): self.getBookById()
         elif (eleccion == 2): self.lendBookByTitle()
         elif (eleccion == 3): self.lendBookByAuthor()
         else: self.generalMenu()
-    
-    def returnBookMenu():
+
+    def returnBookMenu(self):
         print(""" Porfavor digita el id del usuario que va a regresar el libro""")
         userId = input()
         validation = self.validateUser(userId)
-        if(~validation): 
+        if(~validation):
             print("Usuario no existe, volviendo al menu anterior...")
         else: print("devuelto")
-        
+
 
     def lendBookById(id):
         pass
@@ -50,7 +50,7 @@ class main:
 
     def validateUser(self, id):
         pass
- 
+
     def goBack():
         pass
 
@@ -62,11 +62,11 @@ class main:
               """)
         eleccion = int(input())
 
-        if(eleccion == 1): 
+        if(eleccion == 1):
             self.lendBookMenu()
-        elif (eleccion == 2): returnBookMenu()
+        elif (eleccion == 2): self.returnBookMenu()
         else: return
-    
+
     sobreviviendoAEscobar = EBook("0", "Sobreviviendo a Escobar", "JJ", "100", "un libro mas", "3", "2020")
     LaBiblia = EBook("1", "La Santa Biblia", "Diosito", "1000", "el libro de la vida", "100", "0")
     LaBiblia = PhysicalBook("1", "La Santa Biblia", "Diosito", "100", "el libro de la vida", "100", "0", False)
